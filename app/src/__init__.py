cf3f7caad4f66a246f9b86f899e031d499c4a2e5
ImperatorYarik! <yariktb124@gmail.com>
2025-02-16 01:44:02 +0200

    Added user registration feature

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -15,9 +15,11 @@ def create_app():
     from .controllers.home_controller import home_bp
     from .controllers.docker_controller import docker_bp
     from .controllers.setup_controller import setup_bp
+    from .controllers.settings_controller import settings_bp
 
     app.register_blueprint(home_bp)
     app.register_blueprint(docker_bp)
     app.register_blueprint(setup_bp)
+    app.register_blueprint(settings_bp)
 
     return app
